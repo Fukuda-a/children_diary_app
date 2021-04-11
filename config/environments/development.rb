3c2dadb338217eee48e60d0167ff3efdea3214a7
Ubuntu <ubuntu@ip-172-31-35-0.ap-northeast-1.compute.internal>
2021-04-11 07:38:13 +0000

    devise実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
@@ -65,5 +65,18 @@ Rails.application.configure do
   
   
   # mailer setting
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { protocol: 'https', host:'diary_app.c9users.io'}
+  #{ host: 'localhost', port: 3000 }
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'airifkd@gmail.com',
+    :password => 'fynioatatfdrhcaj',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'airifkd@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -307,5 +307,12 @@ Devise.setup do |config|
 
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
-  # config.sign_in_after_change_password = true
+  # config.sign_in_after_change_password = 
+  
+  # lock sessings
+    config.unlock_strategy = :email
+    config.maximum_attempts = 4
+    
+    # timeout setting
+  config.timeout_in = 6.hours
 end

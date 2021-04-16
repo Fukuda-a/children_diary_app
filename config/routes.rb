bccf7854a4ba28b30e86dc8abeb691af7d647dce
Ubuntu <ubuntu@ip-172-31-35-0.ap-northeast-1.compute.internal>
2021-04-16 08:39:00 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   
   root 'pages#index'
   get 'pages/show'
-  resources :children
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

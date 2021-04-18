d28efaa0a2c1fbbac5d422d4dbb274e84127f191
Ubuntu <ubuntu@ip-172-31-35-0.ap-northeast-1.compute.internal>
2021-04-18 02:31:31 +0000

    児童一覧ページ作成と、それのadminユーザーのみの表示

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,5 +1,9 @@
 class ChildrenController < ApplicationController
   
+  def index
+    @children = Child.all
+  end
+  
   def show
    @child = Child.find(params[:id])
   end
@@ -7,5 +11,22 @@ class ChildrenController < ApplicationController
   def new
     @child = Child.new
   end
+  
+  def create
+    @child = Child.new(child_params)
+      if @child.save
+        flash[:success] = "#{@child.name} が登録されました"
+        redirect_to 'children/index'
+      else
+        render 'children/new'
+      end
+  end
+  
+  
+  private
+  
+   def child_params
+     params.require(:child).permit(:name,:birthday,:child_centers_name,:child_number)
+   end
   
 end
--- app/models/child.rb
+++ app/models/child.rb
@@ -1,2 +1,6 @@
 class Child < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :birthday, presence: true
+  validates :child_centers_name, presence: true
+  validates :child_number, presence: true
 end

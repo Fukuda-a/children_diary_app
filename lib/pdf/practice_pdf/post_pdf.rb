ca2aa1986917ed37c7d68983cb4464d27ee49c92
densetsu <tab.23.yude@gmail.com>
2021-05-16 11:37:48 +0900

    PDF機能に日本語フォントが読み込まれるように変更

--- lib/pdf/practice_pdf/post_pdf.rb
+++ lib/pdf/practice_pdf/post_pdf.rb
@@ -1,5 +1,13 @@
 module PracticePdf
   class PostPdf < Prawn::Document
+    FONT = 'app/assets/fonts/SourceHanSans-Normal.ttc'
+    FONT_BOLD = 'app/assets/fonts/SourceHanSans-Bold.ttc'
+    FONT_EXTRALIGHT = 'app/assets/fonts/SourceHanSans-ExtraLight.ttc'
+    FONT_HEAVY = 'app/assets/fonts/SourceHanSans-Heavy.ttc'
+    FONT_LIGHT = 'app/assets/fonts/SourceHanSans-Light.ttc'
+    FONT_MEDIUM = 'app/assets/fonts/SourceHanSans-Medium.ttc'
+    FONT_NORMAL = 'app/assets/fonts/SourceHanSans-Regular.ttc'
+
     def initialize
       super(page_size: 'A4') # 新規PDF作成
       stroke_axis # 座標を表示
@@ -8,7 +16,26 @@ module PracticePdf
     end
     
     def header
+      font FONT
+      text_box 'ヘッダー'
 
+      font FONT_BOLD
+      text_box 'ヘッダー', at: [0, 730], size: 10
+
+      font FONT_EXTRALIGHT
+      text_box 'ヘッダー', at: [0, 700], size: 20
+
+      font FONT_HEAVY
+      text_box 'ヘッダー', at: [0, 650], size: 30
+
+      font FONT_LIGHT
+      text_box 'ヘッダー', at: [0, 600], size: 40
+
+      font FONT_MEDIUM
+      text_box 'ヘッダー', at: [0, 550], size: 50
+
+      font FONT_NORMAL
+      text_box 'ヘッダー', at: [0, 500], size: 60
     end
   end
 end
